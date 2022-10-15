05b5191bd4c82b9eb4221022c3fd986788111a7f
gRaY4449 <onlyforschoolclass378@gmail.com>
2022-10-15 15:01:42 +0900

    課題1完了

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // ArticleSeeder（シーダークラス）の呼び出し
+        $this->call(ArticleSeeder::class);
     }
 }
