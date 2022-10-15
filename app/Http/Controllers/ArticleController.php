37fd0a7c6e375a5de9c0cbaddb36ad150ce9db23
gRaY4449 <onlyforschoolclass378@gmail.com>
2022-10-15 16:56:32 +0900

    論文投稿アプリ課題完了

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -23,7 +23,7 @@ class ArticleController extends Controller
     public function store(Request $request)
     {
         // インスタンスの作成
-        $article= new Article;
+        $article = new Article;
 
         // 値の用意
         $article->title = $request->title;
@@ -41,5 +41,30 @@ class ArticleController extends Controller
     {
         $article = Article::find($id);
         return view('articles.show', ['article' => $article]);
+    }
+
+    public function edit($id)
+    {
+        $article = Article::find($id);
+        return view('articles.edit', ['article' => $article]);
+    }
+    public function update(Request $request, $id)
+    {
+        // ここはidで探して持ってくる以外はstoreと同じ
+        $article = Article::find($id);
+        // 値の用意
+        $article->title = $request->title;
+        $article->body = $request->body;
+        // 保存
+        $article->save();
+        // 登録したらindexに戻る
+        return redirect('/articles');
+    }
+    public function destroy($id)
+    {
+        $article = Article::find($id);
+        $article->delete();
+
+        return redirect('/articles');
     }
 }
